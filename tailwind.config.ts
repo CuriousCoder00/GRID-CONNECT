f55eb5d4121e6f77ee632ed2a8483a2863601381
Kapil Jangid <kapiljangid040@gmail.com>
2024-07-24 01:18:00 +0530

    chore: Update Tailwind CSS configuration file path and add spotlight animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -80,22 +80,11 @@ const config = {
             opacity: "0",
           },
         },
-        spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         "meteor-effect": "meteor 5s linear infinite",
-        spotlight: "spotlight 2s ease .75s 1 forwards",
       },
     },
     backgroundImage: {
