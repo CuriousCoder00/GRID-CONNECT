abfc9ce6a0aa0d4f8b764803152408d589314b84
Kapil Jangid <kapiljangid040@gmail.com>
2024-07-24 01:14:25 +0530

    chore: Add TracingBeam component for animated tracing beam effect

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,6 +1,11 @@
 "use client";
+import { TracingBeam } from "@/components/animated/tracing-beam";
 import { Navbar } from "@/components/Navbar";
+import { Features } from "@/components/static/Features";
 import { Hero } from "@/components/static/Hero";
+import { IconBrandGithub, IconHeart } from "@tabler/icons-react";
+import { LinkedinIcon } from "lucide-react";
+import Link from "next/link";
 
 const links = [
   {
@@ -19,9 +24,26 @@ const links = [
 
 export default function Home() {
   return (
-    <div className="h-full overflow-hidden">
-      <Navbar navLinks={links} />
-      <Hero />
-    </div>
+    <TracingBeam>
+      <div className="h-full overflow-hidden">
+        <Navbar navLinks={links} />
+        <Hero />
+      </div>
+      <Features />
+      <div className="flex container justify-between items-center border-t-2 pt-8">
+        <div className="flex justify-center items-center">&copy; Copyright GridConnects 2024</div>
+        <div className="flex justify-center items-center gap-2">
+          Made with <IconHeart /> by @CuriousCoder
+        </div>
+        <div className="flex justify-center items-center gap-3">
+          <Link scroll={false} href={"/"}>
+            <IconBrandGithub />
+          </Link>
+          <Link scroll={false} href={"/"}>
+            <LinkedinIcon />
+          </Link>
+        </div>
+      </div>
+    </TracingBeam>
   );
 }
