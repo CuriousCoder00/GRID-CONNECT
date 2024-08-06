1094df84c1d123abc81106e03e14969b603e88b2
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-06 15:55:21 +0530

    feat: Improve page load performance with lazy loading for images

--- components/static/Cards.tsx
+++ components/static/Cards.tsx
@@ -7,123 +7,128 @@ const Cards = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
-        <div className="flex object-cover overflow-hidden rounded-md aspect-video">
+        <div className="flex object-cover overflow-hidden rounded-md aspect-video md:w-72 md:h-52 sm:w-48 sm:h-42">
           <Image
+            className="md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20"
             alt="gaming community"
-            width={400}
-            height={50}
             src={pic.WARZONE_COMMUNITY}
           />
         </div>
         <div className="flex w-full flex-col justify-between py-5">
           <div className="flex w-full flex-col">
             <span className="font-bold w-full">Warzone x Call of Duty</span>
-            <span className=" font-light w-full text-sm">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis in praesentium eum numquam provident nisi fuga. Voluptate veniam commodi nostrum!
+            <span className="max-sm:hidden font-light w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis in praesentium eum numquam provident nisi fuga.
+              Voluptate veniam commodi nostrum!
             </span>
           </div>
-          <Gradient/>
+          <Gradient />
           <span className="text-sm">Verified</span>
         </div>
       </div>
       <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
-        <div className="flex overflow-hidden rounded-md aspect-video">
+        <div className="flex overflow-hidden rounded-md aspect-video md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20">
           <Image
             alt="gaming community"
-            width={400}
-            height={50}
+            className="md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20"
             src={pic.GAMING_COMMUNITY}
           />
         </div>
         <div className="flex w-full flex-col justify-between py-5">
           <div className="flex w-full flex-col">
             <span className="font-bold w-full">Gaming Community</span>
-            <span className=" font-light w-full text-sm">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis in praesentium eum numquam provident nisi fuga. Voluptate veniam commodi nostrum!
+            <span className="max-sm:hidden font-light w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis in praesentium eum numquam provident nisi fuga.
+              Voluptate veniam commodi nostrum!
             </span>
           </div>
           <span className="text-sm">Verified</span>
         </div>
       </div>
-      <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
-        <div className="flex overflow-hidden rounded-md aspect-video">
+      <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-2">
+        <div className="flex overflow-hidden rounded-md aspect-video md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20">
           <Image
             alt="gaming community"
-            width={400}
-            height={50}
+            className="md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20"
             src={pic.ESPORTS_COMMUNITY}
           />
         </div>
-        <Gradient/>
+        <Gradient />
         <div className="flex w-full flex-col justify-between py-5">
           <div className="flex w-full flex-col">
             <span className="font-bold w-full">Esports Community</span>
-            <span className=" font-light w-full text-sm">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis in praesentium eum numquam provident nisi fuga. Voluptate veniam commodi nostrum!
+            <span className="max-sm:hidden font-light w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis in praesentium eum numquam provident nisi fuga.
+              Voluptate veniam commodi nostrum!
             </span>
           </div>
           <span className="text-sm">Verified</span>
         </div>
       </div>
       <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
-        <div className="flex overflow-hidden rounded-md aspect-video">
+        <div className="flex overflow-hidden rounded-md aspect-video md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20">
           <Image
             alt="gaming community"
-            width={400}
-            height={50}
+            className="md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20"
             src={pic.TRADING_COMMUNITY}
           />
         </div>
         <div className="flex w-full flex-col justify-between py-5">
           <div className="flex w-full flex-col">
             <span className="font-bold w-full">Trading Community</span>
-            <span className=" font-light w-full text-sm">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis in praesentium eum numquam provident nisi fuga. Voluptate veniam commodi nostrum!
+            <span className="max-sm:hidden font-light w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis in praesentium eum numquam provident nisi fuga.
+              Voluptate veniam commodi nostrum!
             </span>
           </div>
           <span className="text-sm">Verified</span>
         </div>
       </div>
       <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
-        <div className="flex object-fit overflow-hidden rounded-md aspect-video">
+        <div className="flex object-fit overflow-hidden rounded-md aspect-video md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20">
           <Image
-            alt="gaming community"
-            width={400}
-            height={400}
+            alt="gaming community" className="md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20"
             src={pic.VR_COMMUNITY}
           />
         </div>
         <div className="flex w-full flex-col justify-between py-5">
           <div className="flex w-full flex-col">
             <span className="font-bold w-full">VR Community</span>
-            <span className=" font-light w-full text-sm">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis in praesentium eum numquam provident nisi fuga. Voluptate veniam commodi nostrum!
+            <span className="max-sm:hidden font-light w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis in praesentium eum numquam provident nisi fuga.
+              Voluptate veniam commodi nostrum!
             </span>
           </div>
           <span className="text-sm">Verified</span>
         </div>
       </div>
-      <div className="flex p-3 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
-        <Gradient/>
-        <div className="flex overflow-hidden rounded-md aspect-video">
+      <div className="flex p-1 min-w-full hover:bg-slate-300 rounded dark:hover:bg-slate-900 gap-3">
+        <Gradient />
+        <div className="flex overflow-hidden rounded-md aspect-video md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20">
           <Image
             alt="gaming community"
-            width={400}
-            height={50}
+            className="md:w-72 md:h-52 sm:w-48 sm:h-42 w-24 h-20"
             src={pic.LEARNING_COMMUNITY}
           />
         </div>
         <div className="flex w-full flex-col justify-between py-5">
           <div className="flex w-full flex-col">
             <span className="font-bold w-full">Learning Community</span>
-            <span className=" font-light w-full text-sm">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis in praesentium eum numquam provident nisi fuga. Voluptate veniam commodi nostrum!
+            <span className="max-sm:hidden font-light w-full text-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Veritatis in praesentium eum numquam provident nisi fuga.
+              Voluptate veniam commodi nostrum!
             </span>
           </div>
           <span className="text-sm">Verified</span>
         </div>
       </div>
-      <Gradient/>
+      <Gradient />
     </div>
   );
 };
