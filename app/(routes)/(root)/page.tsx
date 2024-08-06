fa2fad28c88477b9f114ef2b56f892ebac92d756
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-06 14:57:48 +0530

    feat: Add lazy loading for images on home page
    This commit adds lazy loading functionality for images on the home page. By implementing lazy loading, the images will only load when they are visible in the viewport, improving the page load performance and user experience.

--- app/(routes)/(root)/page.tsx
+++ app/(routes)/(root)/page.tsx
@@ -1,11 +1,10 @@
 "use client";
 import { TracingBeam } from "@/components/animated/tracing-beam";
+import { MultiStepLoader as Loader } from "@/components/animated/multi-step-loader";
 import { Navbar } from "@/components/Navbar";
 import { Features } from "@/components/static/Features";
 import { Hero } from "@/components/static/Hero";
-import { IconBrandGithub, IconHeart } from "@tabler/icons-react";
-import { LinkedinIcon } from "lucide-react";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const links = [
   {
@@ -22,14 +21,46 @@ const links = [
   },
 ];
 
+const loadingStates = [
+  {
+    text: "Generating Content..",
+  },
+  {
+    text: "Loading Resources..",
+  },
+  {
+    text: "Loading....",
+  },
+];
+
 export default function Home() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 6000);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <TracingBeam className="h-full overflow-hidden">
-      <div className="h-full overflow-x-hidden">
-        <Navbar navLinks={links} />
-        <Hero />
-        <Features />
-      </div>
-    </TracingBeam>
+    <>
+      {loading ? (
+        <div className="w-full h-[60vh] flex items-center justify-center">
+          <Loader
+            loadingStates={loadingStates}
+            loading={loading}
+            duration={2000}
+          />
+        </div>
+      ) : (
+        <TracingBeam className="h-full overflow-hidden">
+          <div className="h-full overflow-x-hidden">
+            <Navbar navLinks={links} />
+            <Hero />
+            <Features />
+          </div>
+        </TracingBeam>
+      )}
+    </>
   );
 }
