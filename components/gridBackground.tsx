fe70bef89db9c316d9f66ff8c89fa7d3e08c03ad
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-24 02:15:49 +0530

    refactor: Update GridBackground component height for better visual display
    This commit updates the height of the GridBackground component in the `gridBackground.tsx` file to improve the visual display. The height value has been changed from `50rem` to `60rem` to provide a larger grid background. This change enhances the overall design and user experience of the application.

--- components/gridBackground.tsx
+++ components/gridBackground.tsx
@@ -7,7 +7,7 @@ type bgProps = {
 export function GridBackground({ children }: bgProps) {
 
   return (
-    <div className="h-[50rem] dark:bg-slate-950 w-full dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
+    <div className="h-[60rem] dark:bg-slate-950 w-full dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center">
       <div className="absolute dark:bg-slate-950 pointer-events-none inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       <div className="relative z-20 mt-12">
         {children}
