f2ddd78e5227b44327141d72b4a82a0bd2954dbb
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-25 18:25:01 +0530

    refactor: Update page layout and styling for registration form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
+import { APP_NAME } from "@/constants/TextConsts";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Grid Connect - A Community of your interests",
   description:
-    "Explore diverse communities tailored to variouswith GridConnect - A Community of your interests. Experience the power of community with GridConnect. Be a part of something bigger.",
+    `Explore diverse communities tailored to variouswith ${APP_NAME} - A Community of your interests. Experience the power of community with ${APP_NAME}. Be a part of something bigger.`,
 };
 
 export default function RootLayout({
