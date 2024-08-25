16f9d785240722647ad22844ae57ab3304952b16
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-25 18:25:33 +0530

    refactor: Update page layout and styling for registration form

--- app/(main_routes)/feed/page.tsx
+++ app/(main_routes)/feed/page.tsx
@@ -1,9 +1,5 @@
 "use client";
-import { TracingBeam } from "@/components/animated/tracing-beam";
-import { MultiStepLoader as Loader } from "@/components/animated/multi-step-loader";
 import { Navbar } from "@/components/Navbar";
-import { Features } from "@/components/static/Features";
-import { Hero } from "@/components/static/Hero";
 import { useEffect, useState } from "react";
 
 const links = [
@@ -36,8 +32,25 @@ const links = [
 
 export default function page() {
   return (
-    <div className="h-full overflow-x-hidden">
-      <Navbar navLinks={links} />
+    <div className="flex flex-1">
+      <div className="p-2 md:p-10 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-slate-900 flex flex-col gap-2 flex-1 w-full h-full">
+        <div className="flex gap-2">
+          {[...new Array(4)].map((i) => (
+            <div
+              key={"first-array" + i}
+              className="h-20 w-full rounded-lg  bg-gray-100 dark:bg-slate-800 animate-pulse"
+            ></div>
+          ))}
+        </div>
+        <div className="flex gap-2 flex-1">
+          {[...new Array(2)].map((i) => (
+            <div
+              key={"second-array" + i}
+              className="h-full w-full rounded-lg  bg-gray-100 dark:bg-slate-800 animate-pulse"
+            ></div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
