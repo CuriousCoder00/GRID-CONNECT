21f702a0d0e700f39175c0889562ba6345e245cf
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-27 18:54:25 +0530

    refactor: Improve registration route by adding user creation and validation
    This commit refactors the registration route in the app/api/auth/register/route.ts file. The changes include:
    - Adding user creation logic with hashed password using bcryptjs
    - Validating email and password inputs
    - Checking if the user already exists with the provided email or username
    - Creating a new user in the database with the necessary fields
    - Returning the created user with a 201 status code
    
    These changes enhance the security and functionality of the registration process, ensuring that user information is properly validated and stored.

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,31 +1,47 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import bcrypt from "bcryptjs";
 import { db } from "@/lib/db";
 import { UserTypes } from "../(types)";
 
-export async function POST(req: NextRequest) {
-  const { name, email, password }: UserTypes = await req.json();
-  // Validate email and password
-  if (!email || !password) {
-    return NextResponse.json(
-      { error: "Email and password are required" },
-      { status: 400 }
-    );
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const { name = "", email, password }: UserTypes = await req.json();
+    // Validate email and password
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
+    }
+    // check if user already
+    const userExists = await db.user.findFirst({
+      where: {
+        OR: [{ email }, { username: email.split("@")[0] }],
+      },
+    });
+    if (userExists) {
+      return NextResponse.json(
+        { error: "User already exists with this email or username" },
+        { status: 400 }
+      );
+    }
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Register user
+    const uname = email.split("@")[0];
+    const user = await db.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        username: uname,
+        imageUrl: "",
+        verifyToken: "",
+        forgotPasswordToken: "",
+      },
+    });
+    return NextResponse.json({ user }, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(error.message);
   }
-  // Register user
-  const user = await db.user.create({
-    data: {
-      name,
-      email,
-      password,
-      userId: "",
-      username: "",
-      imageUrl: "",
-      forgotPasswordToken: "",
-      forgotPasswordTokenExpiry: "",
-      verifyToken: "",
-      verifyTokenExpiry: "",
-    },
-  });
-  return NextResponse.json({ user }, { status: 201 });
 }
