9a48121f7675c621aeeda5f1c5495374813e8313
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-30 15:35:25 +0530

    refactor: Add RESET_PASS template for password reset email

---
+++ helpers/mailer/TEMPLATES/RESET_PASS.ts
@@ -0,0 +1,36 @@
+import { APP_NAME } from "@/constants/TextConsts";
+import emailStyles from "./emailStyles";
+
+const resetPassMailHTML = (resetPasswordLink: string, userName: string) => `
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
+    xmlns:o="urn:schemas-microsoft-com:office:office">
+
+<head>
+    <title>${APP_NAME} reset password email</title><!--[if !mso]>-->
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"><!--<![endif]-->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light">
+    <meta name="supported-color-schemes" content="light">
+    <!--[if mso]><xml><o:OfficeDocumentSettings><o:AllowPNG/><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml><![endif]-->
+    ${emailStyles}
+</head>
+<body>
+    <div class="main-body">
+        <img src="/public/image.png" alt="grid connect logo">
+        <div class="email-container">
+            <!-- Reset Password -->
+            <h1>Reset your password</h1>
+            <p>Hi ${userName},</p>
+            <p>We received a request to reset your password. If you did not make this request, please ignore this email.</p>
+            <p>Click the link below to reset your password.</p>
+            <a href="${resetPasswordLink}">Reset password</a>
+            <p>If you have any questions, please don't hesitate to contact us at: support@${APP_NAME}.com
+            </p>
+        </div>
+    </div>
+</body>
+</html>`;
+
+export default resetPassMailHTML;
