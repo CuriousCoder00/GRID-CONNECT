4b96ca3a2f1f10cced181a30d3b0ea9bdee2fcc1
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-07 07:19:30 +0530

    refactor: Remove unused fields from UserTypes interface

--- app/api/auth/(types)/index.ts
+++
@@ -1,9 +0,0 @@
-export interface UserTypes {
-  name: string;
-  email: string;
-  password: string;
-  username: string;
-  imageUrl: string;
-  verifyToken: string;
-  forgotPasswordToken: string;
-}
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface UserTypes {
+  name: string | null;
+  email: string | null;
+  password: string | null;
+  username: string | null;
+  imageUrl: string | null;
+  verifyToken: string | null;
+  forgotPasswordToken: string | null;
+}
