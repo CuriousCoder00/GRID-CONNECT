636df2fbc5555a54b9f4e4ced2dbb72b9d18fe18
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-08 12:31:10 +0530

    refactor: Add user data functions
    Added functions to retrieve user data by email, ID, account ID, and verification token. These functions use the database connection to query the respective tables and return the corresponding data. This refactor improves code organization and reusability.

---
+++ data/user-data.ts
@@ -0,0 +1,55 @@
+import { db } from "@/lib/db";
+
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { email } });
+    return user;
+  } catch {
+    return null;
+  }
+};
+
+export const getUserByID = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({ where: { id } });
+    return user;
+  } catch {
+    return null;
+  }
+};
+
+export const getAccountByUserId = async (userId: string) => {
+  try {
+    const account = await db.account.findFirst({
+      where: { userId },
+    });
+
+    return account;
+  } catch {
+    return null;
+  }
+};
+
+export const getVerificationTokenByEmail = async (email: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: { email },
+    });
+
+    return verificationToken;
+  } catch {
+    return null;
+  }
+};
+
+export const getVerificationTokenByToken = async (token: string) => {
+  try {
+    const verificationToken = await db.verificationToken.findFirst({
+      where: { token },
+    });
+
+    return verificationToken;
+  } catch {
+    return null;
+  }
+};
