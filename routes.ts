bdbb71897c0dcab1306cfaa8b08c81d0e273fdd3
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-08 12:23:59 +0530

    feat: Add public and authentication routes
    Add an array of routes that are accessible to the public and do not require authentication/login. Also, include an array of routes used for authentication, which will redirect users to the /feed route. Additionally, define the prefix for API authentication routes and set the default redirect path after logging in.

---
+++ routes.ts
@@ -0,0 +1,23 @@
+/**
+ * An array of routes that are accessible to public
+ * These routes do not need authentication/login
+ */
+export const publicRoutes = ["/", "/explore", "/verify-email"];
+
+/**
+ * An array of routes that are used for authentication
+ * These routes do not need authentication/login
+ * These routes will redirect users to /feed route
+ */
+export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that starts with this prefix are used for API authentication purposes
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after logged in
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/feed";
