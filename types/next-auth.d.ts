8a7b0d282b9a425dc833a15ef864074c4d600f3c
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-08 12:28:07 +0530

    refactor: Add ExtendedUser type to next-auth session

---
+++ types/next-auth.d.ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth";
+
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+  isTwoFactorEnabled: boolean;
+  isOAuth: boolean;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
