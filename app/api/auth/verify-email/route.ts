cf3c9aa3b4df62dac8f86f24606eec0538d25371
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-09 00:45:39 +0530

    feat: Add verify email route with email verification logic and success message

---
+++ app/api/auth/verify-email/route.ts
@@ -0,0 +1,27 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { getUserByEmail, getVerificationTokenByToken } from "@/data/user-data";
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { token } = reqBody;
+
+    const existingToken = await getVerificationTokenByToken(token);
+    if (!existingToken)
+      return NextResponse.json({ error: "Token has expired!" });
+    const existingUser = await getUserByEmail(existingToken.email);
+    if (!existingUser)
+      return NextResponse.json({ error: "Email does not exist" });
+    await db.user.update({
+      where: { id: existingUser.id },
+      data: { emailVerified: new Date() },
+    });
+    await db.verificationToken.delete({
+      where: { id: existingToken.id },
+    });
+    return NextResponse.json({ success: "Email verified!" });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
