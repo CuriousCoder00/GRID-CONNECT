c36e0725b9b8a88aa55b2e804cbd5b127d0fd3b0
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-08 12:27:59 +0530

    refactor: Update import paths for authentication components

---
+++ auth.config.ts
@@ -0,0 +1,37 @@
+import type { NextAuthConfig, User } from "next-auth";
+
+import Google from "next-auth/providers/google";
+import credentials from "next-auth/providers/credentials";
+
+import bcrypt from "bcryptjs";
+import { getUserByEmail } from "./data/user-data";
+
+export default {
+  providers: [
+    Google({
+      id: "google",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials): Promise<User | null> {
+        const user = await getUserByEmail(credentials.email as string);
+        if (!user) throw new Error("User not found");
+        const isValid = await bcrypt.compare(
+          credentials.password as string,
+          user.password as string
+        );
+        if (!isValid) throw new Error("Invalid credentials");
+        if (!user.emailVerified)
+          throw new Error("Please verify your email and then try again");
+        return user as User;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
---
+++ auth.ts
@@ -0,0 +1,68 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+
+import { db } from "@/lib/db";
+import authConfig from "@/auth.config";
+
+import { getAccountByUserId, getUserByID } from "./data/user-data";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: {
+          emailVerified: new Date(),
+        },
+      });
+    },
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== "credentials") return true;
+      const existingUser = await getUserByID(user.id!);
+      if (!existingUser?.emailVerified) return false;
+      return true;
+    },
+
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+
+      if (session.user) {
+        session.user.name = token.name;
+        session.user.email = token.email ?? "";
+        session.user.isOAuth = token.isOAuth;
+      }
+
+      return session;
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserByID(token.sub);
+      if (!existingUser) return token;
+      const existingAccount = await getAccountByUserId(existingUser.id);
+
+      token.isOAuth = !!existingAccount;
+      token.name = existingUser.name;
+      token.email = existingUser.email;
+      token.role = existingUser.role;
+
+      return token;
+    },
+  },
+  adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
