8e81a3b0e6796bf1aa2c55a4eed0c862b00e887b
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-10 19:05:12 +0530

    Add TODO component for under construction page

---
+++ components/TODO.tsx
@@ -0,0 +1,16 @@
+import { IconArrowLeft } from "@tabler/icons-react";
+import React from "react";
+
+const TODO = () => {
+  return (
+    <div className="fixed inset-0 bg-black flex flex-col items-center justify-center">
+      <div className="text-2xl">Under construction...</div>
+      <a className="flex justify-center items-center gap-2 text-sm" href="/">
+        <IconArrowLeft className="text-xs" />
+        Back Home
+      </a>
+    </div>
+  );
+};
+
+export default TODO;
