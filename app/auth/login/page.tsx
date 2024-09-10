f410286949157c97d24190077594b54fe7c744d7
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-10 19:04:55 +0530

    refactor: Update styles and layout of LoginPage component

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,7 +5,7 @@ import React, { Suspense } from "react";
 
 export default function LoginPage() {
   return (
-    <div className="max-w-md h-full w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
+    <div className="max-w-md h-full w-full rounded-none md:rounded-2xl shadow-input bg-black">
       <Suspense>
         <LoginForm />
       </Suspense>
