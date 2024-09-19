6750869e14f1600936841c105b772d58c98de870
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-19 21:53:38 +0530

    Refactor explore layout: Add CommunityNavs component

--- app/(routes)/explore/layout.tsx
+++ app/(routes)/explore/layout.tsx
@@ -1,6 +1,8 @@
 import { Gradient } from "@/components/Landing/Gradient";
 import { Navbar } from "@/components/Header/Navbar";
 import Banner from "@/components/Custom/Banner";
+import CommunityNavs from "@/components/Custom/CommunityNavs";
+
 
 const links = [
   {
@@ -15,12 +17,21 @@ const links = [
 
 const ExploreLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full flex items-center justify-center max-sm:overflow-hidden">
-      <div className="flex flex-col">
+    <div className="h-full flex flex-col items-center justify-center max-sm:overflow-hidden">
+      <Banner />
+      <div className="flex flex-col relative">
         <Navbar navLinks={links} showInput={true} />
         <Gradient />
-        <Banner />
-        <div className="w-full h-full z-10 mt-12">{children}</div>
+        <div className="container w-full h-full z-10 md:my-12 my-5 sticky top-12 min-h-[85vh] max-h-[85vh]">
+          <div className="lg:w-[65vw] grid grid-cols-12 mt-12 ">
+            <div className="lg:col-span-3 col-span-12">
+              <CommunityNavs />
+            </div>
+            <div className="lg:col-span-9 gap-3 col-span-12 overflow-y-auto transition-all duration-150 max-h-[80vh]">
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
