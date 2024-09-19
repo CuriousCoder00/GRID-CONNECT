19e88e0fd4af8947c5043072357cd551a7812e22
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-19 21:53:45 +0530

    Refactor explore layout: Remove CommunityNavs component from page

--- app/(routes)/explore/page.tsx
+++ app/(routes)/explore/page.tsx
@@ -6,13 +6,8 @@ import CommunityNavs from "@/components/Custom/CommunityNavs";
 
 const page = () => {
   return (
-    <div className="container w-full grid grid-cols-12 mt-12 ">
-      <div className="sm:col-span-5 md:col-span-3 col-span-12">
-        <CommunityNavs />
-      </div>
-      <div className="sm:col-span-7 md:col-span-9 flex flex-col w-full justify-center items-start gap-3 col-span-12">
-        <Cards />
-      </div>
+    <div className="flex flex-col justify-start items-start">
+      <Cards />
     </div>
   );
 };
