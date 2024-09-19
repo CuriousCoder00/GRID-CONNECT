f7712de158ced7e08dda4b72eeaeb69bac6ed4ee
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-19 15:02:11 +0530

    Refactor mobile navigation and add MobileNavbar component

---
+++ components/Header/NavbarLinks.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import { Url } from "next/dist/shared/lib/router/router";
+import { Input } from "@/components/ui/input";
+
+interface NavbarLinksProps {
+  links?: Array<{ name: String; href: Url }>;
+  showInput?: Boolean;
+}
+
+export const NavbarLinks = ({ links, showInput }: NavbarLinksProps) => {
+  return (
+    <div className="flex md:flex-row md:justify-end md:items-center md:gap-4 flex-col justify-center items-start gap-8 w-full">
+      {showInput && (
+        <Input
+          className="md:w-64 w-full border-[1px]"
+          type="search"
+          placeholder="Explore communities..."
+        />
+      )}
+      {links?.map((item, index) => {
+        return (
+          <Link
+            key={index}
+            scroll={false}
+            href={item.href}
+            className="dark:hover:text-cyan-400
+                    dark:text-slate-300 transition-all delay-200 duration-200 ease-in-out text-slate-700 hover:text-slate-900"
+          >
+            {item.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
