a28d3859910ec3d07de2f91f70387061faf9579b
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-22 00:26:15 +0530

    Refactor Sidebar component: Remove unused imports and update dependencies

--- components/app-components/Sidebar/Sidebar.tsx
+++ components/app-components/Sidebar/Sidebar.tsx
@@ -1,9 +1,5 @@
 "use client";
-import { cn } from "@/lib/utils";
-import Link, { LinkProps } from "next/link";
-import React, { useState, createContext, useContext } from "react";
-import { AnimatePresence, motion } from "framer-motion";
-import { IconMenu2, IconX } from "@tabler/icons-react";
+import React, { useState } from "react";
 
 import { IconBrandTabler } from "@tabler/icons-react";
 import { Sidebar, SidebarBody, SidebarLink } from "@/components/animated/sidebar";
