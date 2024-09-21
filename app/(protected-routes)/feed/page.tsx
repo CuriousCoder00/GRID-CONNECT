41236f072199290fd90ada0f557144957ecdd587
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-21 23:46:08 +0530

    Refactor feed page component: Rename default export to Feed

--- app/(protected-routes)/feed/page.tsx
+++ app/(protected-routes)/feed/page.tsx
@@ -3,7 +3,7 @@
 import logout from "@/actions/logout";
 import { useRouter } from "next/navigation";
 
-export default function page() {
+export default function Feed() {
   const router = useRouter();
 
   const handleLogout = async () => {
@@ -13,7 +13,7 @@ export default function page() {
   return (
     <div className="flex flex-1 h-full w-full">
       <div className="p-2 md:p-10 border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-black flex flex-col gap-2 flex-1 w-full h-full">
-        <div className="flex gap-2">
+        {/*  <div className="flex gap-2">
           {[...new Array(4)].map((i) => (
             <div
               key={i}
@@ -28,9 +28,8 @@ export default function page() {
               className="h-full w-full rounded-lg  bg-gray-100 dark:bg-slate-950 animate-pulse"
             ></div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
-    // <button onClick={handleLogout}>Logout</button>
   );
 }
