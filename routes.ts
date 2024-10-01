4108227d07a3bb358dd984157302f57bb7f3f278
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-01 10:34:28 +0530

    Update default login redirect path

--- routes.ts
+++ routes.ts
@@ -26,4 +26,4 @@ export const apiAuthPrefix = "/api/auth";
 /**
  * The default redirect path after logged in
  */
-export const DEFAULT_LOGIN_REDIRECT = "/chats";
+export const DEFAULT_LOGIN_REDIRECT = "/feed";
