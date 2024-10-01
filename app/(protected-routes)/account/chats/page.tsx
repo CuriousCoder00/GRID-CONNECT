1b17433aa528c32c0beb4d75b8dec38448edfd08
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-01 17:41:10 +0530

    Refactor code: Add navigation breadcrumbs to account management pages

--- app/(protected-routes)/account/chats/page.tsx
+++ app/(protected-routes)/account/chats/page.tsx
@@ -1,8 +1,20 @@
 import TODO from "@/components/TODO";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
-  return <TODO />;
+  return (
+    <div className="flex flex-col w-full h-full max-h-[90vh]">
+      <div className="text-xs">
+        <Link className="mr-1" href="/account">
+          Account
+        </Link>
+        &gt; Chat Settings
+      </div>
+      <div className="flex flex-col mt-5">
+      </div>
+    </div>
+  );
 };
 
 export default Page;
