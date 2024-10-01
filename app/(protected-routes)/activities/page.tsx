e50508492abc6e19014835e59db3dbeb21d6f53d
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-01 10:35:11 +0530

    Refactor code: Remove UserActivities component and update Activities page

--- app/(protected-routes)/activities/page.tsx
+++ app/(protected-routes)/activities/page.tsx
@@ -1,22 +1,94 @@
 "use client";
-
+import React from "react";
+import { Separator } from "@/components/ui/separator";
 import { useEffect, useState } from "react";
-import { UserActivities } from "./_components/UserActivities";
-import { TrendingCommunities } from "./_components/TrendingComms";
+import { ActivityCard } from "./_components/ActivityCard";
+import { getUserActivities } from "@/actions/user-activites";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { LucideListFilter } from "lucide-react";
 
-export default function Feed() {
+type Props = {};
+const Activities = () => {
+  const user = useCurrentUser();
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [activities, setActivities] = useState([] as any[]);
+
+  useEffect(() => {
+    getUserActivities(user.email)
+      .then((data) => {
+        setIsLoaded(true);
+        setActivities(data.activities || []);
+      })
+      .catch((error) => {
+        setIsLoaded(true);
+        setError(error);
+      });
+  }, []);
+
   return (
-    <div className="flex h-full w-full p-5">
-      <div className="relative flex flex-col w-full overflow-y-auto gap-1 px-6 py-2 mt-5">
-        <UserActivities />
-      </div>
-      <div className="flex flex-col gap-2 sm:border-l-2 dark:sm:border-l-slate-800 sm:border-l-slate-200 ps-6 max-sm:hidden">
-        <div className="flex flex-col justify-start items-center">
-          <div className="w-52 h-screen">
-            <TrendingCommunities />
-          </div>
+    <div className="flex flex-col w-full p-2 my-5 px-8">
+      <div className="flex justify-between items-center my-2 w-full">
+        <h1 className="text-xl w-52">Your activities</h1>
+        <div className="flex items-center">
+          <LucideListFilter className="text-neutral-700 dark:text-neutral-200 size-4 flex-shrink-0" />
+          <Select>
+            <SelectTrigger className="focus:outline-none border-none">
+              <SelectValue placeholder="All" defaultValue={"all"} />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                {options.map((option) => (
+                  <SelectItem key={option.value} value={option.value}>
+                    {option.label}
+                  </SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
+      </div>
+      <Separator orientation="horizontal" className="bg-slate-600" />
+      <div className="flex flex-col my-4 gap-4">
+        {isLoaded && activities.length === 0 ? (
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            <h1 className="text-lg">No activities found</h1>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-4">
+            {activities.map((activity) => (
+              <ActivityCard
+                id={activity.id}
+                title={activity.title}
+                description={activity.description}
+                date={activity.date}
+              />
+            ))}
+          </div>
+        )}
+
+        {error && (
+          <div className="flex flex-col items-center justify-center w-full h-full">
+            <h1 className="text-lg">{error}</h1>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default Activities;
+
+const options = [
+  { value: "all", label: "All" },
+  { value: "community", label: "Community activities" },
+  { value: "user", label: "User activities" },
+];
