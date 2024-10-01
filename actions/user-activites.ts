ec39d7142082b97ee98bab434afad3ebe8c02d60
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-01 10:35:20 +0530

    Refactor code: Remove UserActivities component and update Activities page

---
+++ actions/user-activites.ts
@@ -0,0 +1,43 @@
+"use server";
+
+import { getUserByEmail } from "@/data/user-data";
+import { db } from "@/lib/db";
+
+export const getUserActivities = async (email: string) => {
+  const user = await getUserByEmail(email);
+  if (!user) return { error: "User not found" };
+  const activities = await db.activity.findMany({
+    where: {
+      userId: user.id,
+    },
+  });
+  if (!activities) return { error: "Activities not found" };
+  return { activities };
+};
+
+export const storeUserActivity = async (email: string, activity: any) => {
+  const user = await getUserByEmail(email);
+  if (!user) return { error: "User not found" };
+  const newActivity = await db.activity.create({
+    data: {
+      ...activity,
+      user: {
+        connect: {
+          id: user.id,
+        },
+      },
+    },
+  });
+  return { newActivity };
+};
+
+export const getRecentActivities = async () => {
+  const activities = await db.activity.findMany({
+    take: 5,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  if (!activities) return { error: "Activities not found" };
+  return { activities };
+};
