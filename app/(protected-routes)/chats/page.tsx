cc821d56caf195f0f8499c62c39b6540e5055ed7
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-08 18:45:39 +0530

    Refactor code: Update chat page to include chat header component

--- app/(protected-routes)/chats/page.tsx
+++ app/(protected-routes)/chats/page.tsx
@@ -1,77 +1,28 @@
 import React from "react";
 import { ChatSidebar } from "@/components/app-components/chats/sidebar";
 import { Handshake } from "lucide-react";
+import { Header } from "@/components/app-components/chats/chat-header";
 
 const Chat = () => {
   return (
     <div className="flex flex-col w-full h-full max-h-[90vh] gap-1">
-      <div className="flex flex-col gap-3 max-sm:hidden">
-        {[...new Array(600)].map((i) => (
-          <div
-            key={i}
-            className="flex h-4 w-full rounded  bg-gray-100 dark:bg-slate-700"
-          >
-            .
-          </div>
-        ))}
+      <div className="flex flex-col">
+        <Header type="chat" />
+      </div>
+      <div className="flex flex-col h-full w-full overflow-y-auto px-4">
+        <div className="flex flex-col gap-3 max-sm:hidden">
+          {[...new Array(600)].map((i) => (
+            <div
+              key={i}
+              className="flex h-4 w-full rounded  bg-gray-100 dark:bg-slate-700"
+            >
+              .
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Chat;
-
-const Links = [
-  {
-    label: "Friends",
-    href: "/chats/friends",
-    icon: <Handshake className="size-4" />,
-  },
-];
-
-const users = [
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-  {
-    name: "John Doe",
-    avatar: "https://randomuser.me/api/portraits/men/32.jpg",
-  },
-];
