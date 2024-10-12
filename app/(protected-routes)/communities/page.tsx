716c6f5a51f3224030cb70447deb59f27ae78ed1
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-12 23:39:09 +0530

    Refactor code: Update CommunitiesPage component

--- app/(protected-routes)/communities/page.tsx
+++ app/(protected-routes)/communities/page.tsx
@@ -1,26 +1,26 @@
-import React from "react";
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { auth } from "@/lib/auth";
+import { InitSetup } from "@/components/app-components/InitSetup";
 
-const Chat = () => {
-  return (
-    <div className="flex w-full h-full max-h-[90vh] gap-1">
-      <div className="flex flex-col h-full overflow-y-auto hidden-scrollbar gap-2 sm:p-2 md:p-1 w-14 sm:w-20 md:w-18 ">
-        {[...new Array(22)].map((i) => (
-          <div
-            key={i}
-            className="aspect-square flex md:w-16 md:h-16 sm:h-14 sm:w-14 h-12 w-12 rounded-lg bg-gray-300 dark:bg-slate-700"
-          ></div>
-        ))}
-      </div>
-      <div className="flex flex-col h-full w-full overflow-y-auto gap-3 p-2 border-l-2 dark:border-l-slate-700">
-        {[...new Array(600)].map((i) => (
-          <div
-            key={i}
-            className="flex h-4 w-full rounded  bg-gray-100 dark:bg-slate-700"
-          ></div>
-        ))}
-      </div>
-    </div>
-  );
+const CommunitiesPage = async () => {
+  const session = await auth();
+  const user = session?.user;
+  const community = await db.community.findFirst({
+    where: {
+      members: {
+        some: {
+          userId: user.id,
+        },
+      },
+    },
+  });
+
+  if (community) {
+    return redirect(`/communities/${community.id}`);
+  }
+
+  return <InitSetup />;
 };
 
-export default Chat;
+export default CommunitiesPage;
