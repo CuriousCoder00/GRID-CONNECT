d921470c73d337b2632a25f51fc87028b878f4da
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-12 23:39:13 +0530

    Refactor code: Update CommunitiesPage layout component

--- app/(protected-routes)/communities/layout.tsx
+++ app/(protected-routes)/communities/layout.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import { CommunitSidebar } from "@/components/app-components/communities/community-sidebar";
+import React from "react";
 
-const layout = ({children}:{children: React.ReactNode;}) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className='flex w-full h-full overflow-hidden'>
-      {children}
+    <div className="flex w-full h-full overflow-hidden">
+      <div className="flex w-full h-full gap-1">
+        <CommunitSidebar />
+        <div className="flex flex-col h-full w-full overflow-y-auto gap-3 p-2 border-l-2 dark:border-l-slate-700">
+          {children}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
