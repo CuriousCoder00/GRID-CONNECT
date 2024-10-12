c3226330bbbe5aa166919c201adf88875f2af7c0
Kapil Jangid <kapiljangid040@gmail.com>
2024-10-13 01:21:17 +0530

    Refactor code: Add CommunityHeader component for displaying community name and dropdown menu

--- app/(protected-routes)/communities/[communityId]/page.tsx
+++ app/(protected-routes)/communities/[communityId]/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client";
+import { CommunityHeader } from "@/components/app-components/communities/community-header";
+import { usePathname } from "next/navigation";
+import React, { Suspense } from "react";
 
 const CommunityPage = () => {
+  const path = usePathname();
+  const id = path.split("/")[2];
   return (
     <div>
-      hey
+      <Suspense>
+        <CommunityHeader id={id} />
+      </Suspense>
     </div>
-  )
-}
+  );
+};
 
-export default CommunityPage
+export default CommunityPage;
